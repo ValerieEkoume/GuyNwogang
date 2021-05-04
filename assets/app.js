1e8d3f928a7a52d835024c1d104e431b2e00a9e8
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2021-05-04 10:07:53 +0200

    Creation entity Concert + css Gear section

--- assets/app.js
+++ assets/app.js
@@ -17,8 +17,6 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 
-
-
 // Permet l'affichage du nom de l'image sélectionnée dans Create et Edit
 $('.custom-file-input').on('change', function (e) {
     var inputFile = e.currentTarget;
@@ -29,26 +27,26 @@ const tlm = gsap.timeline({defaults: {ease: "power1.out"}});
 tlm.fromTo("nav", {opacity: 0}, { opacity: 1, duration: 1});
 tlm.fromTo("#sign", {opacity: 0}, { opacity: 1, duration: 1.5});
 
-// gsap.registerPlugin(ScrollTrigger);
-//
-// let tl = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: '.about',
-//         start: "top center",
-//         toggleActions: "restart none none none",
-//
-//
-//     }
-// });
-//
-//
-//
-//
-// tl.from(".imgabo", { x: 200, opacity: 0, duration: 1.5})
-//     .from(".content", { y: 300, opacity: 0, duration: 1}, '-=1')
-//
-//
-//
-// // tl.from(".content_gears", { y: 300, opacity: 0, duration: 1})
-// //     .from(".img_gears", { x: 200, opacity: 0, duration: 1.5})
-//
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.about',
+        start: "top center",
+        toggleActions: "restart none none none",
+
+
+    }
+});
+
+
+
+
+tl.from(".imgabo", { x: 200, opacity: 0, duration: 1.5})
+    .from(".content", { y: 300, opacity: 0, duration: 1}, '-=1')
+
+
+
+tl.from(".content_gears", { y: 300, opacity: 0, duration: 1})
+    .from(".img_gears", { x: 200, opacity: 0, duration: 1.5})
+
---
+++ src/Entity/Concert.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConcertRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ConcertRepository::class)
+ */
+class Concert
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $artiste;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pays;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $liens;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArtiste(): ?string
+    {
+        return $this->artiste;
+    }
+
+    public function setArtiste(string $artiste): self
+    {
+        $this->artiste = $artiste;
+
+        return $this;
+    }
+
+    public function getDate(): ?string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(string $pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    public function getLiens(): ?string
+    {
+        return $this->liens;
+    }
+
+    public function setLiens(?string $liens): self
+    {
+        $this->liens = $liens;
+
+        return $this;
+    }
+}
